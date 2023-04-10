aa37db61f6fa832680ff0a5d2da18e6591a9708e
DmytroYaskov <yaskov.dmitro@gmail.com>
2023-04-10 10:38:57 +0300

    Updated argument parsing logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use clap::{Args, Parser, Subcommand};
+use serde::Serialize;
+use chrono_systemd_time::{ parse_timestamp_tz, InvalidTimestamp};
+use chrono;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
-struct Cli {
+struct CLI {
     #[command(subcommand)]
     command: Option<Commands>,
 }
@@ -18,29 +21,82 @@ enum Commands {
 #[derive(Args)]
 struct ConfigureArgs {
     /// Provider name
-    // #[arg(short, long)]
     provider: String,
+
+    /// To update api key
+    #[arg(long)]
+    api: Option<String>,
+
+    /// To update address for which weahter information should be received 
+    #[arg(short, long)]
+    address: Option<String>
 }
 
 #[derive(Args)]
 struct GetArgs {
-    address: String,
+    #[arg(short, long)]
+    address: Option<String>,
+
+    #[arg(short, long, default_value_t = String::from("now"))]
+    time: String,
+    // time: chrono::DateTime<chrono::Utc>,
+}
+
+fn parse_time(arg: &str) -> Result<chrono::DateTime<chrono::Utc>, InvalidTimestamp> {
+    let seconds = parse_timestamp_tz(arg, chrono::Utc)?;
+    Ok(seconds)
+}
+
+#[derive(Serialize)]
+struct ProviderConfiguration {
+    api_key: String
+}
+
+#[derive(Serialize)]
+struct Providers {
+    openweather: Option<ProviderConfiguration>,
+    weatherapi: Option<ProviderConfiguration>,
+}
+
+// #[derive(Serialize)]
+// enum APIProviders {
+//     OpenWeatherMap(String),
+//     WeatherAPI(String),
+// }
+
+#[derive(Serialize)]
+struct AppConfiguration {
+    address: Option<String>,
+    preferred_provider: Option<String>,
+    providers: Providers
 }
 
 fn main() {
-    let cli = Cli::parse();
+    let cli = CLI::parse();
 
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
         Some(Commands::Configure(cfg)) => {
-            println!("Provider: {:?}", cfg.provider)
+            println!("Provider: {:?}", cfg.provider);
+
+            if let Some(api) = cfg.api.as_deref() {
+                println!("Value for api: {api}");
+            }
+
+            if let Some(address) = cfg.address.as_deref() {
+                println!("Value for address: {address}");
+            }
         }
         Some(Commands::Get(cfg)) => {
-            println!("Address: {:?}", cfg.address)
+            println!("Address: {:?}", cfg.address);
+
+            let parsed_time = parse_time(cfg.time.as_str()).unwrap();
+
+            println!("Time: {:?}", parsed_time);
         }
         _ => {
-            println!("Hello, world!")
+            println!("Hello, world!");
         }
     }
 }
